cb583701ebefe6cb9f2db08b85d494d1f5dc3e8b
Richardison Korp <105463563+RichardisonKorp@users.noreply.github.com>
2024-05-17 19:07:38 -0300

    tela_perfil_usuario
    Corrigindo tela de perfil

--- app/(tabs)/add.tsx
+++ app/(tabs)/add.tsx
@@ -5,7 +5,7 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
-export default function ChatboxScreen() {
+export default function AddScreen() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
